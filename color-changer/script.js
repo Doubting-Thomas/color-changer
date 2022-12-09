4cb8b3c6f201e2c8b8d26de94f386e042cd38263
Doubting-Thomas <thomasmichaelayo@gmail.com>
2022-12-09 03:24:02 -0800

    feat: add functionality for hex colors

--- color-changer/script.js
+++ color-changer/script.js
@@ -1,15 +1,27 @@
 "use strict";
 
 const btnSolid = document.querySelector(".btn-solid");
+const btnHex = document.querySelector(".btn-hex");
 const body = document.querySelector("body");
 const heading = document.querySelector(".heading-secondary");
 // Array of colors
-const solidColor = ["red", "blue", "yellow", "aquamarine", "pink"];
+const solidColors = ["red", "blue", "yellow", "aquamarine", "pink"];
+const hexColors = ["#e81cff", "#ff930f", "#fff95b", "#696eff", "#ff8989"];
 
-function changeBackgroundColor() {
-  const colorOptions = parseInt(Math.random() * solidColor.length);
-  body.style.backgroundColor = solidColor[colorOptions];
-  heading.textContent = `You picked the ${solidColor[colorOptions]} color!`;
+// Function for solid colors
+function changeSolidColor() {
+  const colorOptions = parseInt(Math.random() * solidColors.length);
+  body.style.backgroundColor = solidColors[colorOptions];
+  heading.textContent = `You picked the ${solidColors[colorOptions]} color!`;
 }
 
-btnSolid.addEventListener("click", changeBackgroundColor);
+// Function for hex colors
+function changeHexColor() {
+  const colorPicker = parseInt(Math.random() * hexColors.length);
+  body.style.backgroundColor = hexColors[colorPicker];
+  heading.textContent = `You picked a hex color - ${hexColors[colorPicker]}!`;
+}
+
+// Event listeners for swapping colors
+btnSolid.addEventListener("click", changeSolidColor);
+btnHex.addEventListener("click", changeHexColor);

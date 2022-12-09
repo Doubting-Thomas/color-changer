af0db39f970516464f600bc2028bb92d9f234dce
Doubting-Thomas <thomasmichaelayo@gmail.com>
2022-12-09 03:46:50 -0800

    feat: add functionality for resetting colors

--- color-changer/script.js
+++ color-changer/script.js
@@ -4,6 +4,7 @@ const btnSolid = document.querySelector(".btn-solid");
 const btnHex = document.querySelector(".btn-hex");
 const body = document.querySelector("body");
 const heading = document.querySelector(".heading-secondary");
+const btnReset = document.querySelector(".btn-reset");
 // Array of colors
 const solidColors = ["red", "blue", "yellow", "aquamarine", "pink"];
 const hexColors = ["#e81cff", "#ff930f", "#fff95b", "#696eff", "#ff8989"];
@@ -13,6 +14,7 @@ function changeSolidColor() {
   const colorOptions = parseInt(Math.random() * solidColors.length);
   body.style.backgroundColor = solidColors[colorOptions];
   heading.textContent = `You picked the ${solidColors[colorOptions]} color!`;
+  btnReset.classList.remove("hidden");
 }
 
 // Function for hex colors
@@ -20,8 +22,15 @@ function changeHexColor() {
   const colorPicker = parseInt(Math.random() * hexColors.length);
   body.style.backgroundColor = hexColors[colorPicker];
   heading.textContent = `You picked a hex color - ${hexColors[colorPicker]}!`;
+  btnReset.classList.remove("hidden");
 }
 
 // Event listeners for swapping colors
 btnSolid.addEventListener("click", changeSolidColor);
 btnHex.addEventListener("click", changeHexColor);
+btnReset.addEventListener("click", function () {
+  btnReset.classList.add("hidden");
+  body.style.backgroundColor = "";
+  heading.textContent =
+    "You have reset to the default background! Click below to change!";
+});
